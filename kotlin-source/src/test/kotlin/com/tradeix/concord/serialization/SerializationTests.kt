dc04c3f4c5ff75df092ae549e7c0e7e2309641a2
Grant Nunn <grant.nunn@outlook.com>
2017-11-09 13:00:51 +0000

    <MPD-98> - Add addtional serialization test for TradeAssetIssuanceRequestMessage which includes the X500 field.

--- kotlin-source/src/test/kotlin/com/tradeix/concord/serialization/SerializationTests.kt
+++ kotlin-source/src/test/kotlin/com/tradeix/concord/serialization/SerializationTests.kt
@@ -1,6 +1,7 @@
 package com.tradeix.concord.serialization
 
 import com.google.gson.GsonBuilder
+import com.tradeix.concord.messages.TradeAssetIssuanceRequestMessage
 import net.corda.core.identity.CordaX500Name
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -36,6 +37,20 @@ class SerializationTests {
         assertEquals("London", result.conductor.locality)
         assertEquals("TradeIX", result.conductor.organisation)
     }
+
+    @Test
+    fun `Corda X500Name should deserialize as part of TradeAssetIssuanceRequestMessage`(){
+        val testRequestMessageJson = "{\"externalId\":\"1\",\"buyer\":\"CN=Buyer One, OU=ABC, O=Buyer1, L=London, S=Whitehall, C=GB\",\"supplier\":\"CN=Supplier One, OU=ABC, O=Supplier1, L=London, S=Whitehall, C=GB\",\"conductor\":\"CN=Conductor One, OU=ABC, O=Conductor1, L=London, S=Whitehall, C=GB\",\"status\":\"Test\",\"value\":100.0,\"currency\":\"GBP\",\"attachmentId\":\"c026064d-1c4d-44d8-8932-d47205a7f863\",\"correlationId\":\"fe3f3cd7-4ab3-41d8-8b5d-705667b8e7c1\",\"tryCount\":0}"
+
+        val serializer = GsonBuilder()
+                .registerTypeAdapter(CordaX500Name::class.java, CordaX500NameDeserializer())
+                .disableHtmlEscaping()
+                .create()
+
+        val result = serializer.fromJson<TradeAssetIssuanceRequestMessage>(testRequestMessageJson, TradeAssetIssuanceRequestMessage::class.java)
+        assertEquals("London", result.buyer?.locality)
+    }
+
 
     data class ConductorTestMessage(val conductor: CordaX500Name)
 }
