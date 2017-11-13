949544b62fbc5832c66cdd9e44d76a5f4f143f0b
Matthew Layton <matthew.layton@live.co.uk>
2017-11-13 12:10:39 +0000

    MPD-192 changed tryCount to 0

--- kotlin-source/src/main/kotlin/com/tradeix/concord/messages/rabbit/RabbitRequestMessage.kt
+++ kotlin-source/src/main/kotlin/com/tradeix/concord/messages/rabbit/RabbitRequestMessage.kt
@@ -2,5 +2,5 @@ package com.tradeix.concord.messages.rabbit
 
 abstract class RabbitRequestMessage(
         override val correlationId: String?,
-        open val tryCount: Int = 1
+        open val tryCount: Int = 0
 ) : RabbitMessage(correlationId)
