9a6b8bbc0ecf11b8cc64d8f35e14a0dcd71239a5
Matthew Layton <matthew.layton@live.co.uk>
2017-11-16 14:46:54 +0000

    MPD-202 updated project settings.

--- kotlin-source/src/test/kotlin/com/tradeix/concord/Main.kt
+++ kotlin-source/src/test/kotlin/com/tradeix/concord/Main.kt
@@ -1,6 +1,5 @@
 package com.tradeix.concord
 
-import com.tradeix.concord.services.messaging.TixMessageSubscriptionStartup
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.utilities.getOrThrow
 import net.corda.node.services.transactions.ValidatingNotaryService
@@ -8,75 +7,53 @@ import net.corda.nodeapi.User
 import net.corda.nodeapi.internal.ServiceInfo
 import net.corda.testing.driver.driver
 
-fun main(args: Array<String>) {
-    // No permissions required as we are not invoking flows.
-    val user = User("user1", "test", permissions = setOf())
-    // Create RabbitMQ subscriptions
-    // TixMessageSubscriptionStartup.exec()
-    driver(isDebug = true) {
+class Main {
+    companion object {
 
-       // Notary
-        startNode(
-                providedName = CordaX500Name("R3Net", "London", "GB"),
-                advertisedServices = setOf(ServiceInfo(ValidatingNotaryService.type))
-        )
+        // RPC Users
+        private val USER = User("user1", "test", permissions = setOf())
 
-        // Conductor
-        startWebserver(
+        // Nodes
+        private val NOTARY = CordaX500Name("R3Net", "London", "GB")
+        private val CONDUCTOR = CordaX500Name("TradeIX", "London", "GB")
+
+        private val BUYER_1 = CordaX500Name("Exxon Mobil", "Irving", "US")
+        private val BUYER_2 = CordaX500Name("Boeing", "Chicago", "US")
+
+        private val SUPPLIER_1 = CordaX500Name("Royal Dutch Shell", "The Hague", "NL")
+        private val SUPPLIER_2 = CordaX500Name("General Electric", "Boston", "US")
+
+        private val FUNDER_1 = CordaX500Name("BNY Mellon", "New York", "US")
+        private val FUNDER_2 = CordaX500Name("JP Morgan Chase", "New York", "US")
+
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            driver(isDebug = true) {
+
+                // Notary
                 startNode(
-                        providedName = CordaX500Name("TradeIX", "London", "GB"),
-                        rpcUsers = listOf(user)
-                ).getOrThrow()
-        )
+                        providedName = NOTARY,
+                        advertisedServices = setOf(ServiceInfo(ValidatingNotaryService.type)))
 
-        // Buyer 1
-        startWebserver(
-                startNode(
-                        providedName = CordaX500Name("Exxon Mobil", "Irving Texas", "US"),
-                        rpcUsers = listOf(user)
-                ).getOrThrow()
-        )
+                val peerNodes = listOf(
+                        CONDUCTOR,
+                        BUYER_1,
+                        BUYER_2,
+                        SUPPLIER_1,
+                        SUPPLIER_2,
+                        FUNDER_1,
+                        FUNDER_2
+                )
 
-        // Buyer 2
-        startWebserver(
-                startNode(
-                        providedName = CordaX500Name("Boeing", "Chicago Illinois", "US"),
-                        rpcUsers = listOf(user)
-                ).getOrThrow()
-        )
+                peerNodes.forEach {
+                    startWebserver(startNode(
+                            providedName = it,
+                            rpcUsers = listOf(USER)).getOrThrow())
+                }
 
-        // Supplier 1
-        startWebserver(
-                startNode(
-                        providedName = CordaX500Name("Royal Dutch Shell", "The Hague", "NL"),
-                        rpcUsers = listOf(user)
-                ).getOrThrow()
-        )
-
-        // Supplier 2
-        startWebserver(
-                startNode(
-                        providedName = CordaX500Name("General Electric", "Boston Massachusetts", "US"),
-                        rpcUsers = listOf(user)
-                ).getOrThrow()
-        )
-
-        // Funder 1
-        startWebserver(
-                startNode(
-                        providedName = CordaX500Name("BNY Mellon", "New York", "US"),
-                        rpcUsers = listOf(user)
-                ).getOrThrow()
-        )
-
-        // Funder 2
-        startWebserver(
-                startNode(
-                        providedName = CordaX500Name("JP Morgan Chase", "New York", "US"),
-                        rpcUsers = listOf(user)
-                ).getOrThrow()
-        )
-
-        waitForAllNodesToFinish()
+                waitForAllNodesToFinish()
+            }
+        }
     }
 }
