4d7bbea5480d1d526bc319505d999e5fe1915ff8
Anthony Jones <jaffamol@gmail.com>
2017-11-23 11:17:56 +0000

    MPD-203 - Moved queue subscription into a class to keep things more tidy
    - Created FlowQueuesSubscriber interface
    - Created IssuanceFlowQueuesSubscriber class
    - Call IssuanceFlowQueuesSubscriber class from startup

--- kotlin-source/src/main/kotlin/com/tradeix/concord/services/messaging/TixMessageSubscriptionStartup.kt
+++ kotlin-source/src/main/kotlin/com/tradeix/concord/services/messaging/TixMessageSubscriptionStartup.kt
@@ -5,6 +5,7 @@ import com.rabbitmq.client.ConnectionFactory
 import com.tradeix.concord.interfaces.IQueueConsumer
 import com.tradeix.concord.messages.rabbit.RabbitMessage
 import com.tradeix.concord.messages.rabbit.tradeasset.TradeAssetIssuanceRequestMessage
+import com.tradeix.concord.services.messaging.subscribers.IssuanceFlowQueuesSubscriber
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigParseOptions
 import com.typesafe.config.ConfigRenderOptions
@@ -28,55 +29,20 @@ class TixMessageSubscriptionStartup(val services: CordaRPCOps) {
         val  defaultConfig = ConfigFactory.parseResources("tix.integration.conf", ConfigParseOptions.defaults().setAllowMissing(false))
         private fun initializeQueues(cordaRpcService: CordaRPCOps) {
             try {
-                if (currentConsumers.count() == 0) {
-                    println("Initializing RabbitMQ Subscriptions - this should happen only once")
-                    val serializer = Gson()
-                    val connectionConfig = defaultConfig!!.resolve().getConfig("tix-integration.rabbitMqConnectionConfiguration").parseAs<RabbitMqConnectionConfiguration>()
+                val serializer = Gson()
+                val connectionConfig = defaultConfig!!.resolve().getConfig("tix-integration.rabbitMqConnectionConfiguration").parseAs<RabbitMqConnectionConfiguration>()
+                val connectionFactory = ConnectionFactory()
 
-                    val consumerConfigurationString = defaultConfig.resolve().getConfig("tix-integration").getObject("issuanceConsumeConfiguration").render(ConfigRenderOptions.concise())
-                    val issueConsumeConfiguration = serializer.fromJson(consumerConfigurationString, RabbitConsumerConfiguration::class.java)
+                connectionFactory.username = connectionConfig.userName
+                connectionFactory.password = connectionConfig.password
+                connectionFactory.host = connectionConfig.hostName
+                connectionFactory.virtualHost = connectionConfig.virtualHost
+                connectionFactory.port = connectionConfig.portNumber
 
-                    val deadLetterConfigurationString = defaultConfig.resolve().getConfig("tix-integration").getObject("issuanceDeadLetterConfiguration").render(ConfigRenderOptions.concise())
-                    val deadLetterConfig =  serializer.fromJson(deadLetterConfigurationString, RabbitDeadLetterConfiguration::class.java)
+                val connectionProvider = RabbitMqConnectionProvider(connectionFactory)
 
-                    val connectionFactory = ConnectionFactory()
-
-                    connectionFactory.username = connectionConfig.userName
-                    connectionFactory.password = connectionConfig.password
-                    connectionFactory.host = connectionConfig.hostName
-                    connectionFactory.virtualHost = connectionConfig.virtualHost
-                    connectionFactory.port = connectionConfig.portNumber
-
-                    val connectionProvider = RabbitMqConnectionProvider(connectionFactory)
-
-                    val transactionResponseConfigurationString = defaultConfig.resolve().getConfig("tix-integration").getObject("transactionResponseConfiguration").render(ConfigRenderOptions.concise())
-                    val transactionResponseConfiguration = serializer.fromJson(transactionResponseConfigurationString, RabbitProducerConfiguration::class.java)
-
-                    val responderConfigurations = mapOf("cordatix_response" to transactionResponseConfiguration)
-
-                    val deadLetterProducer = RabbitDeadLetterProducer<RabbitMessage>(
-                            deadLetterConfiguration = deadLetterConfig,
-                            rabbitConnectionProvider = connectionProvider
-                    )
-
-                    val messageConsumerFactory = MessageConsumerFactory(
-                            services = cordaRpcService,
-                            responderConfigurations = responderConfigurations,
-                            rabbitConnectionProvider = connectionProvider
-                    )
-
-                    val tradeIssuanceConsumer = RabbitMqConsumer(
-                            rabbitConsumerConfiguration = issueConsumeConfiguration,
-                            messageClass = TradeAssetIssuanceRequestMessage::class.java,
-                            deadLetterProducer = deadLetterProducer,
-                            rabbitConnectionProvider = connectionProvider,
-                            messageConsumerFactory = messageConsumerFactory
-                    )
-
-                    tradeIssuanceConsumer.subscribe()
-                    currentConsumers.put(issueConsumeConfiguration.queueName, tradeIssuanceConsumer)
-                    println("RabbitMQ subscriptions done")
-                }
+                IssuanceFlowQueuesSubscriber(cordaRpcService, defaultConfig, serializer)
+                        .initialize(connectionProvider, currentConsumers)
 
             } catch (ex: Throwable) {
                 log.error(ex.message)
