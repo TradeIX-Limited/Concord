05616d691a460994c48cdeb943c0f081caf69b2a
Grant Nunn <grant.nunn@outlook.com>
2017-12-04 10:45:15 +0000

    <MPD-349> - Add deployment step to copy nodes across to the Azure Large server.

--- Deployment/3-deploy.sh
+++ Deployment/3-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 tar -zcvf nodes.tar.gz ./kotlin-source/build/nodes
-scp -i ~/testnet_keys/priv_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/nodes_$(date +%F-%H-%M-%S).tar.gz
+scp -i ~/testnet_keys/priv_azurelarge_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/nodes_$(date +%F-%H-%M-%S).tar.gz
