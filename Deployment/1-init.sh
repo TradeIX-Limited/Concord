f1fa71334e870e8c6ec91ceae4271b2bcd11ea78
Grant Nunn <grant.nunn@outlook.com>
2017-12-05 17:14:04 +0000

    <MPD-350> - build script changes.

--- Deployment/1-init.sh
+++ Deployment/1-init.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+cp tix.integration.template.conf tix.integration.conf
+    sed -i -- "s|{{Username}}|${RABBIT_LOGIN_USR}|g" tix.integration.conf
+    sed -i -- "s|{{Password}}|${RABBIT_LOGIN_PSW}|g" tix.integration.conf
+    sed -i -- "s|{{RabbitPort}}|5672|g" tix.integration.conf
+    sed -i -- "s|{{RabbitHost}}|${rabbit_machineip}|g" tix.integration.conf
--- Deployment/3-deploy.sh
+++ Deployment/3-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 tar -zcvf nodes.tar.gz ./tradeix-concord/build/nodes
-scp -i ~/testnet_keys/priv_azurelarge_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/nodes_$(date +%F-%H-%M-%S).tar.gz
+scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/nodes_$buildenv_$(date +%F-%H-%M-%S).tar.gz
