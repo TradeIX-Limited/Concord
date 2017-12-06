f66e90ce21f51bd8127853f04536b957f6716f29
Grant Nunn <grant.nunn@outlook.com>
2017-12-06 13:30:01 +0000

    <MPD-350> - Push artefacts into separate folders as per code review. Also rename jar file with included tag name so this is refelected in the initial set of nodes.

--- Deployment/3-deploy.sh
+++ Deployment/3-deploy.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-tar -zcvf nodes.tar.gz ./tradeix-concord/build/nodes
 tagname=marcopolo-release-${BUILD_DATE%$'\n'}
 
-echo "scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/nodes_${buildenv}_${tagname}.tar.gz"
-scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/nodes_${buildenv}_${tagname}.tar.gz
-echo "scp -i $deploy_target_key ./tradeix-concord/build/libs/tradeix-concord-*.jar build@40.68.115.203:/home/build/corda_builds/$buildenv/tradeix-concord-${buildenv}_${tagname}.jar"
-scp -i $deploy_target_key ./tradeix-concord/build/libs/tradeix-concord-*.jar build@40.68.115.203:/home/build/corda_builds/$buildenv/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/libs/tradeix-concord-*.jar ./tradeix-concord/build/libs/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/TradeIX/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIX/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/TradeIXTestBuyer/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestBuyer/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/TradeIXTestSupplier1/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier1/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/TradeIXFakeSupplier/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXFakeSupplier/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/R3Net/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/R3Net/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/TradeIXTestSupplier/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/TradeIXTestFunder/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestFunder/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+mv ./tradeix-concord/build/nodes/TradeIXTestSupplier2/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier2/plugins/tradeix-concord-${buildenv}_${tagname}.jar
+
+tar -zcvf nodes.tar.gz ./tradeix-concord/build/nodes
+
+scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/initial/nodes_${buildenv}_${tagname}.tar.gz
+scp -i $deploy_target_key ./tradeix-concord/build/libs/tradeix-concord-${buildenv}_${tagname}.jar build@40.68.115.203:/home/build/corda_builds/$buildenv/incremental/tradeix-concord-${buildenv}_${tagname}.jar
