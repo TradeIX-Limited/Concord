a767288687741dbcfe9b1f1014149887ae82ce90
Grant Nunn <grant.nunn@outlook.com>
2017-12-06 11:22:34 +0000

    <MPD-350> - Fix script error.

--- Deployment/3-deploy.sh
+++ Deployment/3-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 tar -zcvf nodes.tar.gz ./tradeix-concord/build/nodes
-tagname=marcopolo-release-$BUILD_DATE
+tagname=marcopolo-release-$BUILD_DATE | tr '\n' ''
+
 echo "scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/nodes_${buildenv}_${tagname}.tar.gz"
 scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/nodes_${buildenv}_${tagname}.tar.gz
 echo "scp -i $deploy_target_key ./tradeix-concord/build/libs/tradeix-concord-*.jar build@40.68.115.203:/home/build/corda_builds/$buildenv/tradeix-concord-${buildenv}_${tagname}.jar"
