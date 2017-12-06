38525763d7169b64ab36da73a3bc465c6e13b0e3
Grant Nunn <grant.nunn@outlook.com>
2017-12-06 11:38:01 +0000

    <MPD-350> - Fix script to remove newline from variable.

--- Deployment/3-deploy.sh
+++ Deployment/3-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 tar -zcvf nodes.tar.gz ./tradeix-concord/build/nodes
-tagname=marcopolo-release-$BUILD_DATE | tr '\n' ''
+tagname=$(echo marcopolo-release-$BUILD_DATE | tr '\n' '')
 
 echo "scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/nodes_${buildenv}_${tagname}.tar.gz"
 scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/nodes_${buildenv}_${tagname}.tar.gz
