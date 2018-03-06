969878ebbfba0425702e35aca15a025742fae74e
Grant Nunn <grant.nunn@outlook.com>
2018-03-06 11:26:36 +0000

    <DEVOPS-23> - Add new build scripts.

---
+++ Deployment/4-publish.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker build --no-cache -t tradeix:Corda .
+echo ${AZ_CONTAINER_REG_LOGIN_PSW} | docker login tradeixdev.azurecr.io --username ${AZ_CONTAINER_REG_LOGIN_USR} --password-stdin
+docker tag tradeix:Corda tradeixdev.azurecr.io/corda:latest
+docker push tradeixdev.azurecr.io/corda:latest
