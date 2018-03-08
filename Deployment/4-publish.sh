cd28d71e19c2e45f56cc7459b82670f0e4828d9f
Grant Nunn <grant.nunn@outlook.com>
2018-03-08 16:57:16 +0000

    <DEVOPS-23> - Update publish script to remove local image before re-building docker image.

--- Deployment/4-publish.sh
+++ Deployment/4-publish.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+docker rmi tradeixdev.azurecr.io/corda:latest
 docker build --no-cache -t tradeix:Corda .
 echo ${AZ_CONTAINER_REG_LOGIN_PSW} | docker login tradeixdev.azurecr.io --username ${AZ_CONTAINER_REG_LOGIN_USR} --password-stdin
 docker tag tradeix:Corda tradeixdev.azurecr.io/corda:latest
