94abc5d841a619bf418689757591ab6e19687737
Grant Nunn <grant.nunn@outlook.com>
2018-03-09 16:38:16 +0000

    <DEVOPS-23> - Update main docker-compose.yml file.

--- Deployment/bring-up-services.sh
+++ Deployment/bring-up-services.sh
@@ -3,6 +3,6 @@ AZ_CONTAINER_REG_LOGIN_USR=$1
 AZ_CONTAINER_REG_LOGIN_PSW=$2
 
 echo ${AZ_CONTAINER_REG_LOGIN_PSW} | docker login tradeixdev.azurecr.io --username ${AZ_CONTAINER_REG_LOGIN_USR} --password-stdin
-docker-compose --file /etc/tradeix/containervolumes/corda/docker-compose.run.yml down
-docker-compose --file /etc/tradeix/containervolumes/corda/docker-compose.run.yml pull --parallel
-docker-compose --file /etc/tradeix/containervolumes/corda/docker-compose.run.yml up -d
+docker-compose down
+docker-compose pull --parallel
+docker-compose up -d
