565e266ebc592e750cddad389ecea30dd2a36312
Grant Nunn <grant.nunn@outlook.com>
2018-03-26 13:57:23 +0100

    <DEVOPS-43> - Add docker_tag to publish and script to bring up services.

--- Deployment/4-publish.sh
+++ Deployment/4-publish.sh
@@ -2,5 +2,5 @@
 docker rmi tradeixdev.azurecr.io/corda:latest
 docker build --no-cache -t tradeix:Corda .
 echo ${AZ_CONTAINER_REG_LOGIN_PSW} | docker login tradeixdev.azurecr.io --username ${AZ_CONTAINER_REG_LOGIN_USR} --password-stdin
-docker tag tradeix:Corda tradeixdev.azurecr.io/corda:latest
+docker tag tradeix:Corda tradeixdev.azurecr.io/corda:$docker_tag
 docker push tradeixdev.azurecr.io/corda:latest
--- Deployment/bring-up-services.sh
+++ Deployment/bring-up-services.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 AZ_CONTAINER_REG_LOGIN_USR=$1
 AZ_CONTAINER_REG_LOGIN_PSW=$2
+export docker_tag=$3
 
 echo ${AZ_CONTAINER_REG_LOGIN_PSW} | docker login tradeixdev.azurecr.io --username ${AZ_CONTAINER_REG_LOGIN_USR} --password-stdin
 docker-compose down
