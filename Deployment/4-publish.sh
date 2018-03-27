a50b1d09a141ceff9aacee027b6e3e17293803bc
Grant Nunn <grant.nunn@outlook.com>
2018-03-27 11:16:31 +0100

    <DEVOPS-47> - Add missing docker_tag variables.

--- Deployment/4-publish.sh
+++ Deployment/4-publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-docker rmi tradeixdev.azurecr.io/corda:latest
+docker rmi tradeixdev.azurecr.io/corda:$docker_tag
 docker build --no-cache -t tradeix:Corda .
 echo ${AZ_CONTAINER_REG_LOGIN_PSW} | docker login tradeixdev.azurecr.io --username ${AZ_CONTAINER_REG_LOGIN_USR} --password-stdin
 docker tag tradeix:Corda tradeixdev.azurecr.io/corda:$docker_tag
-docker push tradeixdev.azurecr.io/corda:latest
+docker push tradeixdev.azurecr.io/corda:$docker_tag
