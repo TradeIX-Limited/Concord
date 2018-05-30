5d7265b3c30d67533fd63fe5f17f3de6900bba07
rajeshso <fun_rajesh@yahoo.com>
2018-05-30 17:13:58 +0100

    fix Jenkins build

--- Deployment/2-build.sh
+++ Deployment/2-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 ./gradlew clean copyTixIntegrationConfiguration build
-./gradlew deployDevNodes
+./gradlew deployNodes
 cp ./tradeix-concord/build/libs/tradeix-concord-0.1.jar /datadrive/share/corda
+cp ./tradeix-concord-domain/build/libs/tradeix-concord-domain-0.1.jar /datadrive/share/corda
