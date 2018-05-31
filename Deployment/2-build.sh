2a7047a7c3a0e671ab96b383abb9ca9a459b4a63
rajeshso <fun_rajesh@yahoo.com>
2018-05-31 10:57:06 +0100

    Clean up the jenkins file

--- Deployment/2-build.sh
+++ Deployment/2-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ./gradlew clean copyTixIntegrationConfiguration build
 ./gradlew deployNodes
-cp ./tradeix-concord/build/libs/tradeix-concord-0.1.jar /datadrive/share/corda
-cp ./tradeix-concord-domain/build/libs/tradeix-concord-domain-0.1.jar /datadrive/share/corda
+#cp ./tradeix-concord/build/libs/tradeix-concord-0.1.jar /datadrive/share/corda
+#cp ./tradeix-concord-domain/build/libs/tradeix-concord-domain-0.1.jar /datadrive/share/corda
--- Deployment/3-deploy.sh
+++ Deployment/3-deploy.sh
@@ -2,18 +2,18 @@
 buildname=${release_name}-${BUILD_DATE%$'\n'}
 
 cp ./tix.integration.conf ./tradeix-concord/build/nodes/TradeIX/tix.integration.conf
-mv ./tradeix-concord/build/libs/tradeix-concord-*.jar ./tradeix-concord/build/libs/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/TradeIX/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIX/plugins/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/TradeIXTestBuyer/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestBuyer/plugins/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/TradeIXTestSupplier1/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier1/plugins/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/TradeIXFakeSupplier/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXFakeSupplier/plugins/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/R3Net/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/R3Net/plugins/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/TradeIXTestSupplier/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier/plugins/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/TradeIXTestFunder/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestFunder/plugins/tradeix-concord-${buildenv}_${buildname}.jar
-mv ./tradeix-concord/build/nodes/TradeIXTestSupplier2/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier2/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/libs/tradeix-concord-*.jar ./tradeix-concord/build/libs/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/TradeIX/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIX/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/TradeIXTestBuyer/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestBuyer/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/TradeIXTestSupplier1/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier1/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/TradeIXFakeSupplier/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXFakeSupplier/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/R3Net/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/R3Net/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/TradeIXTestSupplier/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/TradeIXTestFunder/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestFunder/plugins/tradeix-concord-${buildenv}_${buildname}.jar
+#mv ./tradeix-concord/build/nodes/TradeIXTestSupplier2/plugins/tradeix-concord-*.jar ./tradeix-concord/build/nodes/TradeIXTestSupplier2/plugins/tradeix-concord-${buildenv}_${buildname}.jar
 
 tar -zcvf nodes.tar.gz ./tradeix-concord/build/nodes
 
 scp -i $deploy_target_key nodes.tar.gz build@40.68.115.203:/home/build/corda_builds/$buildenv/initial/nodes_${buildenv}_${buildname}.tar.gz
-scp -i $deploy_target_key ./tradeix-concord/build/libs/tradeix-concord-${buildenv}_${buildname}.jar build@40.68.115.203:/home/build/corda_builds/$buildenv/incremental/tradeix-concord-${buildenv}_${buildname}.jar
+#scp -i $deploy_target_key ./tradeix-concord/build/libs/tradeix-concord-${buildenv}_${buildname}.jar build@40.68.115.203:/home/build/corda_builds/$buildenv/incremental/tradeix-concord-${buildenv}_${buildname}.jar
 scp -i $deploy_target_key ./tix.integration.conf build@40.68.115.203:/home/build/corda_builds/$buildenv/incremental/tix.integration.conf
