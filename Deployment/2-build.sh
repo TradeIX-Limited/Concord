f1e86f70eed2ef75dd1be2892d7244465bfe33a7
rajeshso <fun_rajesh@yahoo.com>
2018-05-31 12:03:59 +0100

    Clean up the jenkins file

--- Deployment/2-build.sh
+++ Deployment/2-build.sh
@@ -3,3 +3,5 @@
 ./gradlew deployNodes
 #cp ./tradeix-concord/build/libs/tradeix-concord-0.1.jar /datadrive/share/corda
 #cp ./tradeix-concord-domain/build/libs/tradeix-concord-domain-0.1.jar /datadrive/share/corda
+
+# test build
