42ec445af40dc9f0cfe1b3c32133026974004b5a
dancronintix <37584792+dancronintix@users.noreply.github.com>
2018-06-05 11:30:33 +0100

    Added -x test

--- Deployment/2-build.sh
+++ Deployment/2-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ./gradlew clean copyTixIntegrationConfiguration build
-./gradlew deployNodes
+./gradlew deployNodes -x test
 #cp ./tradeix-concord/build/libs/tradeix-concord-0.1.jar /datadrive/share/corda
 #cp ./tradeix-concord-domain/build/libs/tradeix-concord-domain-0.1.jar /datadrive/share/corda
 
