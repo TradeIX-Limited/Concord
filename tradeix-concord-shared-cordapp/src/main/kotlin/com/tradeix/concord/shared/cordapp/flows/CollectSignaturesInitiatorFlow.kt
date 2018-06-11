a2606ba33841acc310b55f4e9f842a9a27be091b
Matthew Layton <matthew.layton@live.co.uk>
2018-06-11 11:45:11 +0100

    EI-5-InvoiceIssuance Refactoring Flows in line with Mike Hearn's recommendation. Flow tests now working Demo working.

--- tradeix-concord-shared-cordapp/src/main/kotlin/com/tradeix/concord/shared/cordapp/flows/CollectSignaturesInitiatorFlow.kt
+++ tradeix-concord-shared-cordapp/src/main/kotlin/com/tradeix/concord/shared/cordapp/flows/CollectSignaturesInitiatorFlow.kt
@@ -5,18 +5,21 @@ import net.corda.core.flows.CollectSignaturesFlow
 import net.corda.core.flows.FlowLogic
 import net.corda.core.flows.FlowSession
 import net.corda.core.flows.InitiatingFlow
+import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.ProgressTracker
 
 @InitiatingFlow
 class CollectSignaturesInitiatorFlow(
         private val transaction: SignedTransaction,
-        private val flowSessions: Collection<FlowSession>,
+        private val counterparties: Iterable<Party>,
         private val tracker: ProgressTracker
 ) : FlowLogic<SignedTransaction>() {
 
     @Suspendable
     override fun call(): SignedTransaction {
+        val flowSessions = mutableListOf<FlowSession>()
+        counterparties.forEach { flowSessions.add(initiateFlow(it)) }
         return subFlow(CollectSignaturesFlow(transaction, flowSessions, tracker))
     }
 }
--- tradeix-concord-shared/src/main/kotlin/com/tradeix/concord/shared/extensions/ContractStateExtensions.kt
+++ tradeix-concord-shared/src/main/kotlin/com/tradeix/concord/shared/extensions/ContractStateExtensions.kt
@@ -2,12 +2,38 @@ package com.tradeix.concord.shared.extensions
 
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.Party
+import net.corda.core.node.ServiceHub
 import java.security.PublicKey
 
 fun Iterable<ContractState>.getAllParticipants(): Iterable<AbstractParty> {
-    return this.flatMap { it.participants }.distinct()
+    return this
+            .flatMap { it.participants }
+            .distinct()
+}
+
+fun Iterable<ContractState>.getAllParticipantsExceptMe(serviceHub: ServiceHub): Iterable<AbstractParty> {
+    return this
+            .flatMap { it.participants }
+            .filter { !serviceHub.myInfo.legalIdentities.contains(it) }
+            .distinct()
+}
+
+fun Iterable<ContractState>.getAllWellKnownParticipants(serviceHub: ServiceHub): Iterable<Party> {
+    return this
+            .getAllParticipants()
+            .map { serviceHub.identityService.requireWellKnownPartyFromAnonymous(it) }
+}
+
+fun Iterable<ContractState>.getAllWellKnownParticipantsExceptMe(serviceHub: ServiceHub): Iterable<Party> {
+    return this
+            .getAllParticipantsExceptMe(serviceHub)
+            .map { serviceHub.identityService.requireWellKnownPartyFromAnonymous(it) }
 }
 
 fun Iterable<ContractState>.getAllOwningKeys(): List<PublicKey> {
-    return this.getAllParticipants().map { it.owningKey }.distinct()
+    return this
+            .getAllParticipants()
+            .map { it.owningKey }
+            .distinct()
 }
