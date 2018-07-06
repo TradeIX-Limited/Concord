28ae1165206112abe7b554257e1c9ef58b94d589
Matthew Layton <matthew.layton@live.co.uk>
2018-07-06 16:55:23 +0100

    Added mechanism to allow different .properties files to be loaded by clients.

--- tradeix-concord-shared-client/src/main/kotlin/com/tradeix/concord/shared/client/components/Address.kt
+++ tradeix-concord-shared-client/src/main/kotlin/com/tradeix/concord/shared/client/components/Address.kt
@@ -4,11 +4,15 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
+import org.springframework.context.annotation.PropertySources
 import org.springframework.stereotype.Component
 
 @Component
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySources(
+        PropertySource("classpath:receiver.properties", ignoreResourceNotFound = true),
+        PropertySource("classpath:responder.properties", ignoreResourceNotFound = true)
+)
 class Address(
         @Value("\${config.erp.host}") val host: String,
         @Value("\${config.erp.port}") val port: Int
