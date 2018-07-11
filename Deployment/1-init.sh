302662e2a3d570fad3daa6022cf449e70f7f220b
rajeshso <fun_rajesh@yahoo.com>
2018-07-11 13:47:02 +0100

    Jenkins build

--- Deployment/1-init.sh
+++ Deployment/1-init.sh
@@ -1,12 +1,1 @@
 #!/usr/bin/env bash
-if [ "${buildenv}" == "demo" ]; then
-    tagname=${release_name}-${BUILD_DATE%$'\n'}
-    git tag -a $tagname -m "Tagged by Jenkins from Build Num: ${BUILD_NUMBER}"
-    git push origin $tagname
-fi
-
-cp tix.integration.template.conf tix.integration.conf
-    sed -i -- "s|{{Username}}|${RABBIT_LOGIN_USR}|g" tix.integration.conf
-    sed -i -- "s|{{Password}}|${RABBIT_LOGIN_PSW}|g" tix.integration.conf
-    sed -i -- "s|{{RabbitPort}}|5672|g" tix.integration.conf
-    sed -i -- "s|{{RabbitHost}}|${rabbit_machineip}|g" tix.integration.conf
--- Deployment/2-build.sh
+++ Deployment/2-build.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-./gradlew clean copyTixIntegrationConfiguration build
-./gradlew deployDevNodes
-cp ./tradeix-concord/build/libs/tradeix-concord-0.1.jar /datadrive/share/corda
+./gradlew clean build deployNodes
