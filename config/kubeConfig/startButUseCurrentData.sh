64b2b3c5b0911b7dc549f1b0ca4e0035bdc9e070
rajeshso <fun_rajesh@yahoo.com>
2018-07-19 10:44:29 +0100

    Azure environment fix

--- config/kubeConfig/startButUseCurrentData.sh
+++ config/kubeConfig/startButUseCurrentData.sh
@@ -1,16 +1,35 @@
 #!/usr/bin/env bash
-#set -eu
-source commonutil.sh
 
-#kubectl apply -f storage/
+source commonutil.sh
+source config.sh
+
+
+uploadStorage() {
+  local cnt=${#MODULES[@]}
+  for ((i=0;i<cnt;i++)); do
+    kubectl apply -f storage/${MODULES[i]}.yaml
+    sleep ${SHORT_DELAY}
+    # echo ${PVC_NAMES}
+  done
+}
+uploadApps() {
+  local cnt=${#MODULES[@]}
+  for ((i=0;i<cnt;i++)); do
+    kubectl apply -f services/${MODULES[i]}.yml
+    sleep ${SHORT_DELAY}
+    kubectl apply -f deployments/${MODULES[i]}.yml
+    sleep ${SHORT_DELAY}
+    # echo ${PVC_NAMES}
+  done
+}
+
+#kubectl apply -f storage/AzureFileStorage.yaml
+#uploadStorage
 genPVCNames
 genAzurePVCs ${PVC_NAMES}
 #azUploadPVCAll
-azUploadTixIntegration
 azUploadSecrets
-kubectl apply -f services/
-kubectl apply -f deployments/
+azUploadReceiverResponderProperties
+uploadApps
 kubectl get all
-# REMEMBER TO CHANGE THE -g OPTION TO THE RESOURCE GROUP and THE -n TO THE NAME OF THE AKS NAME
 az aks browse -g rg-azureks -n cordacluster1
-#kubectl get pvc -l storage-tier=corda
