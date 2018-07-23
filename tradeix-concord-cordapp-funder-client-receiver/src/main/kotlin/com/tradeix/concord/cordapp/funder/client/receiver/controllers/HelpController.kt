6b5906a13f21cced4b38a48d75867dcd18337999
Matthew Layton <matthew.layton@live.co.uk>
2018-07-23 15:06:28 +0100

    Updated funder help controller.

--- tradeix-concord-cordapp-funder-client-receiver/src/main/kotlin/com/tradeix/concord/cordapp/funder/client/receiver/controllers/HelpController.kt
+++ tradeix-concord-cordapp-funder-client-receiver/src/main/kotlin/com/tradeix/concord/cordapp/funder/client/receiver/controllers/HelpController.kt
@@ -3,6 +3,7 @@ package com.tradeix.concord.cordapp.funder.client.receiver.controllers
 import com.tradeix.concord.cordapp.funder.messages.fundingresponses.FundingResponseIssuanceRequestMessage
 import com.tradeix.concord.cordapp.funder.messages.fundingresponses.FundingResponseIssuanceResponseMessage
 import com.tradeix.concord.cordapp.funder.validators.fundingresponses.FundingResponseIssuanceRequestMessageValidator
+import com.tradeix.concord.shared.client.messages.fundingresponses.FundingResponseResponseMessage
 import com.tradeix.concord.shared.client.webapi.RequestParameterInfo
 import com.tradeix.concord.shared.client.webapi.ResponseBuilder
 import com.tradeix.concord.shared.domain.contracts.FundingResponseContract
@@ -12,11 +13,28 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
 import java.util.concurrent.Callable
+import javax.xml.ws.Response
 
 @RestController
 @RequestMapping(path = arrayOf("/help"), produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
 class HelpController {
+
+    @GetMapping
+    fun getHelp(): Callable<ResponseEntity<*>> {
+        return Callable {
+            ResponseBuilder.ok(
+                    arrayOf(
+                            mapOf(
+                                    "endpoint" to "/fundingresponses",
+                                    "help" to "/help/fundingresponses"
+                            )
+                        // TODO : Readd missing end-points.
+                    )
+            )
+        }
+    }
 
     @GetMapping(path = arrayOf("/fundingresponses"))
     fun getFundingResponseControllerHelp(): Callable<ResponseEntity<*>> {
@@ -24,10 +42,30 @@ class HelpController {
             try {
                 ResponseBuilder.ok(
                         mapOf(
-                                "externalId" to RequestParameterInfo(false),
-                                "status" to RequestParameterInfo(false, "unconsumed", listOf("unconsumed", "consumed", "all")),
-                                "pageNumber" to RequestParameterInfo(false, "1"),
-                                "pageSize" to RequestParameterInfo(false, "50")
+                                "parameters" to mapOf(
+                                        "externalId" to RequestParameterInfo(false),
+                                        "status" to RequestParameterInfo(
+                                                false,
+                                                "unconsumed",
+                                                listOf("unconsumed", "consumed", "all")
+                                        ),
+                                        "pageNumber" to RequestParameterInfo(false, "1"),
+                                        "pageSize" to RequestParameterInfo(false, "50")
+                                ),
+                                "produces" to listOf(
+                                        FundingResponseResponseMessage(
+                                                "FUNDING_RESPONSE_1",
+                                                null,
+                                                listOf("INV_001", "INV_002"),
+                                                "Supplier X500 Name",
+                                                "Funder X500 Name",
+                                                BigDecimal.valueOf(123.45),
+                                                "GBP",
+                                                BigDecimal.valueOf(123.45),
+                                                BigDecimal.valueOf(123.45),
+                                                BigDecimal.valueOf(123)
+                                        )
+                                )
                         )
                 )
             } catch (ex: Exception) {
