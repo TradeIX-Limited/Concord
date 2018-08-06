fd0af0701514f5334dc4510e477738cd09c5c3a6
Laura Perez Crespo <laurapc@tradeix.com>
2018-08-06 11:09:05 +0100

    erp-eco165
    Corrections

--- tradeix-concord-shared-domain/src/main/kotlin/com/tradeix/concord/shared/domain/enumerations/FundingResponseStatus.kt
+++ tradeix-concord-shared-domain/src/main/kotlin/com/tradeix/concord/shared/domain/enumerations/FundingResponseStatus.kt
@@ -3,8 +3,10 @@ package com.tradeix.concord.shared.domain.enumerations
 import net.corda.core.serialization.CordaSerializable
 
 @CordaSerializable
-enum class FundingResponseStatus {
-    PENDING,
-    ACCEPTED,
-    REJECTED
+enum class FundingResponseStatus(name: String) {
+    PENDING("DecisionPending"),
+    ACCEPTED("Accepted"),
+    REJECTED("Declined");
+
+    override fun toString(): String = name
 }
