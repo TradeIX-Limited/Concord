2f94d4468720f3cebdc4474fbe921801451545ab
Matthew Layton <matthew.layton@live.co.uk>
2018-08-06 11:22:18 +0100

    Removed an unnecessary message class Removed the timer from the notification service.

--- tradeix-concord-cordapp-funder-client-responder/src/main/kotlin/com/tradeix/concord/cordapp/funder/client/responder/services/FundingResponseNotificationService.kt
+++ tradeix-concord-cordapp-funder-client-responder/src/main/kotlin/com/tradeix/concord/cordapp/funder/client/responder/services/FundingResponseNotificationService.kt
@@ -4,8 +4,6 @@ import com.google.gson.GsonBuilder
 import com.tradeix.concord.cordapp.funder.client.responder.components.TIXAuthenticatedHeaderProvider
 import com.tradeix.concord.cordapp.funder.client.responder.components.TIXConfiguration
 import com.tradeix.concord.cordapp.funder.mappers.fundingresponses.FundingResponseNotificationMapper
-import com.tradeix.concord.cordapp.funder.messages.fundingresponses.FundingResponseNotificationMessage
-import com.tradeix.concord.cordapp.funder.messages.fundingresponses.FundingResponseNotificationRequestMessage
 import com.tradeix.concord.shared.client.components.RPCConnectionProvider
 import com.tradeix.concord.shared.client.http.HttpClient
 import com.tradeix.concord.shared.client.services.ObserverService
@@ -17,8 +15,6 @@ import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.core.config.Configurator
 import org.slf4j.Logger
 import org.springframework.http.HttpEntity
-import java.util.*
-import kotlin.concurrent.timer
 
 class FundingResponseNotificationService(
         private val rpcConnectionProvider: RPCConnectionProvider,
@@ -34,37 +30,24 @@ class FundingResponseNotificationService(
     private val client = HttpClient()
     private val serializer = GsonBuilder().getConfiguredSerializer()
     private val mapper = FundingResponseNotificationMapper()
-    private val fundingResponses = mutableListOf<FundingResponseNotificationMessage>()
-
-    private var vaultObserverTimer: Timer? = null
 
     override fun start() {
         vaultService.observe {
-            vaultObserverTimer?.cancel()
-
-            fundingResponses.add(mapper.map(it.state.data))
-
             Configurator.setLevel(logger.name, Level.DEBUG)
             logger.debug("*** FUNDER FUNDING RESPONSE NOTIFICATION SERVICE >> Funding Response external Id: " + it.state.data.linearId.externalId)
 
-            vaultObserverTimer = timer(period = tixConfiguration.vaultObserverTimeout, action = {
-                try {
-                    val json = serializer.toJson(FundingResponseNotificationRequestMessage(fundingResponses))
-                    val url = tixConfiguration.webApiUrl + "v1/fundingresponses"
-                    val response = client.post<Any>(
-                            url,
-                            HttpEntity(json, tixAuthenticatedHeaderProvider.headers))
+            try {
+                val json = serializer.toJson(mapper.map(it.state.data))
+                val url = tixConfiguration.webApiUrl + "v1/fundingresponses"
+                val response = client.post<Any>(
+                        url,
+                        HttpEntity(json, tixAuthenticatedHeaderProvider.headers))
 
-                    logger.info("Status Code: ${response.statusCode}.")
+                logger.info("Status Code: ${response.statusCode}.")
 
-                } catch (ex: Exception) {
-                    logger.error(ex.message)
-                } finally {
-                    fundingResponses.clear() // TODO : What happens if there was an exception thrown?
-                    vaultObserverTimer?.cancel()
-                }
-            })
-
+            } catch (ex: Exception) {
+                logger.error(ex.message)
+            }
         }
     }
 }
--- tradeix-concord-shared-domain/src/main/kotlin/com/tradeix/concord/shared/domain/enumerations/FundingResponseStatus.kt
+++ tradeix-concord-shared-domain/src/main/kotlin/com/tradeix/concord/shared/domain/enumerations/FundingResponseStatus.kt
@@ -3,10 +3,10 @@ package com.tradeix.concord.shared.domain.enumerations
 import net.corda.core.serialization.CordaSerializable
 
 @CordaSerializable
-enum class FundingResponseStatus(name: String) {
+enum class FundingResponseStatus(private val alternativeName: String) {
     PENDING("DecisionPending"),
     ACCEPTED("Accepted"),
     REJECTED("Declined");
 
-    override fun toString(): String = name
+    override fun toString(): String = alternativeName
 }
